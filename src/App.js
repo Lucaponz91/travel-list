e50b8aacefa13403718d29102bc43847734ac02a
Lucaponz91 <lucapo1991@msn.com>
2023-08-02 22:33:19 +0200

    add onDelete

--- src/App.js
+++ src/App.js
@@ -7,11 +7,22 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item){
+    setItems((items)=> [...items, item])
+  }
+
+  function handleDeleteItem(id){
+    console.log(id)
+    setItems(items=>items.filter(item=>item.id !== id))
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -21,9 +32,11 @@ function Logo() {
   return <h1> Far Away⛱</h1>;
 }
 
-function Form() {
+function Form({onAddItems}) {
   const [description, setDescription] = useState("")
-  const [quantity, setQuantity] = useState(1)
+  const [quantity, setQuantity] = useState(1);
+
+  
 
 
   function handleSubmit(e) {
@@ -33,6 +46,8 @@ function Form() {
 
     const newItem = {description, quantity, packed: false, id: Date.now()};
     console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -62,23 +77,23 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({items, onDeleteItem}) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id}/>
+        {items.map((item) => (
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem}/>
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? {textDecoration: "line-through"} : {}}>{item.quantity} {item.description}</span>
-      <button>❌</button>
+      <button onClick={()=>onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
